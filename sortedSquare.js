7b852680b8cf3e9bf2dae51dde16d66d659e0fc9
Charlotte Currey <charlottecurrey7@gmail.com>
2023-07-24 14:59:16 -0400

    solution 1 & 2

--- sortedSquare.js
+++ sortedSquare.js
@@ -1,5 +1,7 @@
 // Write a function that takes in a non-empty array of integers that are sorted in ascending order and returns a new array of the same length with the squares of the original integers also sorted in ascending order.
 
+//solution 1
+
 function sortedSquaredArray(array) {
   // Write your code here.
   const sortedSquares = new Array(array.length);
