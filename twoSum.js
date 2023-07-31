7e9abc90fc7d432cc800b17b9095649a55e20c6b
Charlotte Currey <charlottecurrey7@gmail.com>
2023-07-31 12:56:09 -0400

    leetcode twosum solutions added

--- twoSum.js
+++ twoSum.js
@@ -58,3 +58,32 @@ function twoNumberSum(array, targetSum) {
 
   return [];
 }
+
+// leetcode basic solution
+
+var twoSum = function (nums, target) {
+  let indexes = [];
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        indexes.push(i);
+        indexes.push(j);
+      }
+    }
+  }
+  return indexes;
+};
+
+//leetcode optimized solution
+
+var twoSum = function (nums, target) {
+  const numMap = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (numMap.has(complement)) {
+      return [numMap.get(complement), i];
+    }
+    numMap.set(nums[i], i);
+  }
+  return [];
+};
