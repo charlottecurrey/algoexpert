bdc8817923913ed84fee90fc1784999f7a75d674
Charlotte Currey <122467377+charlottecurrey@users.noreply.github.com>
2023-11-07 11:03:59 -0500

    new problem added

---
+++ evaluateExpressionTree.js
@@ -0,0 +1,26 @@
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function evaluateExpressionTree({ value, left, right }) {
+  // Write your code here.
+  if (value === -1)
+    return evaluateExpressionTree(left) + evaluateExpressionTree(right);
+  if (value === -2)
+    return evaluateExpressionTree(left) - evaluateExpressionTree(right);
+  if (value === -3)
+    return Math.trunc(
+      evaluateExpressionTree(left) / evaluateExpressionTree(right)
+    );
+  if (value === -4)
+    return evaluateExpressionTree(left) * evaluateExpressionTree(right);
+  return value;
+}
+
+// Do not edit the lines below.
+exports.BinaryTree = BinaryTree;
+exports.evaluateExpressionTree = evaluateExpressionTree;
